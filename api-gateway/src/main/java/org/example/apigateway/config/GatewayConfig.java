1c1ec9dae5216d8bcc05f3f0e4dcfc45c7700b4c
Shuma-dev <den.shumakovski@gmail.com>
2026-08-15 14:26:15 +0300

    fresh

--- api-gateway/src/main/java/org/example/apigateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/org/example/apigateway/config/GatewayConfig.java
@@ -1,15 +1,17 @@
 package org.example.apigateway.config;
 
+import java.net.URI;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
 import static org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions.circuitBreaker;
 import static org.springframework.cloud.gateway.server.mvc.filter.LoadBalancerFilterFunctions.lb;
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
-import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
-import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.path;
+import static org.springframework.web.servlet.function.RequestPredicates.path;
 
 @Configuration
 public class GatewayConfig {
@@ -18,11 +20,11 @@ public class GatewayConfig {
     public RouterFunction<ServerResponse> userServiceRoute() {
 
         return route("user-service")
-                .route(path("/users/**"), http())
+                .route(path("/users/**"), HandlerFunctions.http())
                 .filter(lb("USER-SERVICE"))
-                .filter(circuitBreaker(config ->
-                        config.setId("userServiceCircuitBreaker")
-                                .setFallbackUri("forward:/fallback/users")
+                .filter(circuitBreaker(
+                        "userServiceCircuitBreaker",
+                        URI.create("forward:/fallback/users")
                 ))
                 .build();
     }
